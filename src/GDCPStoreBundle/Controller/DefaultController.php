440dde7a90db8784304adf484b99c83780405914
Admin <Admin@LAPTOP-UON34NS1>
2016-08-12 15:20:18 -0500

    Persisting Objects to MongoDB

--- src/GDCPStoreBundle/Controller/DefaultController.php
+++ src/GDCPStoreBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace GDCPStoreBundle\Controller;
 
+use GDCPStoreBundle\Document\Product;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -9,5 +11,18 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('GDCPStoreBundle:Default:index.html.twig');
+    }
+    
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $dm->persist($product);
+        $dm->flush();
+
+        return new Response('Created product id '.$product->getId());
     }
 }
